f3b21d671c8587abd23cda21d854a9b81f4ca210
darshan <darshan.mistry@ezeetechnosys.com>
2022-05-30 12:43:02 +0530

    this is darshan express

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const hostname = '127.0.0.1';
+//packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+//user
+const hostname = "127.0.0.1";
 const port = 5000;
 
+//connection
 
+const con = mysql.createConnection({
+  host: "",
+  user: "",
+  password: "",
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log("db connected.");
+});
+
+const app = express();
 app.use(bodyParser.json());
 
-
-app.get("/",(req,res)=> {
-
+app.get("/", (req, res) => {
   res.send("GuestPortal Backend");
-})
-
-
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
