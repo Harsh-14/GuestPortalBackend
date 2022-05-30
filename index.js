72184c66dab0085dcec7b1856bab459b96353784
Harsh Patel <harsh.patel@ezeetechnosys.com>
2022-05-30 12:19:58 +0530

    change in index file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Guest Portal Backend');
+  res.end('Guest Portal Backendd');
 });
 
 server.listen(port, hostname, () => {
