a90c233e142397898f916d549e39c85591181d71
darshan <darshan.mistry@ezeetechnosys.com>
2022-06-02 12:54:32 +0530

    withooutcors

--- index.js
+++ index.js
@@ -2,34 +2,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const dotenv = require("dotenv").config()
-
+const dotenv = require("dotenv").config();
+const cors = require("cors")
+const localPort = 5000;
 //user
 
-
 //connection
-const host = process.env.host;
+
 const con = mysql.createConnection({
-  host:'192.168.10.11',
-  port:"3306",
-  user:'saas1',
-  password:'saas',
-  connectTimeout:60,
-  
-  
-
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  // connectTimeout: 60,
 });
 
-con.connect((err) => {
-  if (err) throw err;
-  console.log("db connected.");
-});
-
+// con.connect((err) => {
+//   if (err) throw err;
+//   console.log("db connected.");
+// });
 
 //middleware
 const app = express();
+// app.use(cors())
 app.use(bodyParser.json());
-
 
 //routes
 app.get("/", (req, res) => {
@@ -37,7 +33,29 @@ app.get("/", (req, res) => {
 });
 
 
+app.post("/login", async (req, res) => {
+  // console.log("@12")
+  const { loginId, pin } = req.body;
+
+  if (!loginId || !pin) {
+    return res.status(403).json({ error: "Please fill all the fileds." });
+  } else {
+    console.log(loginId, pin);
+
+    // con.query(sql,(req,res) => {
+
+    //   if(err)z
+    //   {
+    //     console.log("Result",error)
+    //   }
+
+    // })
+    
+    return  res.status(200).json({message:"done"})
+  }
+});
+
 //liisten
-app.listen(5000,'127.0.0.1', () => {
-  console.log(`Server running at localhost port 5000 `);
+app.listen(localPort, "127.0.0.1", () => {
+  console.log(`Server running at http://localhost:${localPort}/`);
 });
