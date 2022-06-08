dc24792df8c6357c570cf899ac118a2f966738c9
darshan <darshan.mistry@ezeetechnosys.com>
2022-06-08 13:28:14 +0530

    sql structure

---
+++ SQL/sqlQueries.js
@@ -0,0 +1,95 @@
+exports.checkHotel = `SELECT databasename from saasconfig.syscompany where hotel_code=?`;
+
+exports.checkPin = `SELECT fdtraninfo.tranunkid,fdtraninfo.statusunkid,fdrentalinfo.is_void_cancelled_noshow_unconfirmed AS isVoid,fdtraninfo.isgroupowner,fdtraninfo.allowselfguestportalaccess ,fdguesttran.guestunkid, fdtraninfo.supress_roomcharge from saas_ezee.fdtraninfo LEFT JOIN saas_ezee.fdrentalinfo ON fdtraninfo.tranunkid = fdrentalinfo.tranunkid AND fdrentalinfo.is_void_cancelled_noshow_unconfirmed=IF(fdtraninfo.statusunkid IN(5,6,7),1,0) AND ifnull(fdrentalinfo.ismodifiedfromOTA,0) <> 1 AND fdrentalinfo.statusunkid NOT IN (8,12) AND fdrentalinfo.hotel_code= ? LEFT JOIN saas_ezee.fdguesttran ON fdtraninfo.masterguesttranunkid = fdguesttran.guesttranunkid  AND fdguesttran.hotel_code= ? where IF(IFNULL(fdtraninfo.reservationno,'')='',TRIM(LEADING  FROM (TRIM(LEADING ? FROM fdtraninfo.tranunkid)))= ?,fdtraninfo.reservationno=?) AND 
+fdtraninfo.doorcode=? AND fdtraninfo.hotel_code=? GROUP BY fdtraninfo.tranunkid `;
+
+exports.checkLanguage = `SELECT cfuser.language,cfuser.userunkid from saas_ezee.cfuser where hotel_code=? AND username=? AND sysdefined=?`;
+
+exports.selectHotel_stuff =`SELECT website,accountfor,logo FROM saas_ezee.cfhotel WHERE hotel_code=?`;
+
+exports.check_currency= `SELECT ER.exchangerateunkid,ER.isprefix AS Prefix,ER.currency_sign As Currency,ER.currency_code As Code,digits_after_decimal,ER.currency_name As Currency_Name,ER.exchange_rate1 as exchange_rate1,ER.exchange_rate2 as exchange_rate2 FROM saas_ezee.cfexchangerate AS ER  WHERE ER.isactive = 1 AND ER.hotel_code=?`;
+
+
+exports.userData =`SELECT 
+IFNULL(CASE
+            WHEN IFNULL(fdtraninfo.groupunkid, 0) = 0 THEN fdtraninfo.reservationno
+            ELSE (CASE
+                WHEN IFNULL(fdtraninfo.subreservationno, 0) = 0 THEN fdtraninfo.reservationno
+                ELSE CONCAT(fdtraninfo.reservationno,
+                        '-',
+                        fdtraninfo.subreservationno)
+            END)
+        END,
+        '') AS reservationno,
+TRIM(LEADING '0' FROM (TRIM(LEADING ? FROM fdtraninfo.tranunkid))) AS loginid,
+fdtraninfo.arrivaldatetime AS arrival,
+fdtraninfo.masterfoliounkid AS foliounkid,
+fdtraninfo.departuredatetime AS departure,
+travelagentvoucherno,
+CONCAT(IFNULL(trcontact.salutation, ''),
+        ' ',
+        trcontact.name) AS name,
+trcontact.email AS notiemail,
+fdtraninfo.doorcode AS notidoorcode,
+fdtraninfo.reservationno AS notiresno,
+IFNULL((SELECT 
+                lookupunkid
+            FROM
+                saas_ezee.cfcommonlookup AS cfcommonlookup
+            WHERE
+                hotel_code = ?
+                    AND lookuptype = 'SALUTATION'
+                    AND (cfcommonlookup.lookup = trcontact.salutation)
+            LIMIT 1),
+        '') AS salutation,
+fdrentalinfo.adult,
+fdrentalinfo.child,
+cfroomtype.roomtype AS RoomType,
+cfroom.name AS Roomno,
+fdtraninfo.statusunkid,
+fdtraninfo.isgroupowner,
+fdrentalinfo.ratetypeunkid,
+DATEDIFF(fdtraninfo.departuredatetime,
+        fdtraninfo.arrivaldatetime) AS noofdays,
+CAST(fdtraninfo.arrivaldatetime AS DATE) AS arrivaldate,
+CAST(fdtraninfo.departuredatetime AS DATE) AS departuredate,
+fdrentalinfo.childage,
+fdtraninfo.ispaymentlinkamountset,
+fdtraninfo.googleshortlink,
+fdtraninfo.lang_key,
+fdtraninfo.sourceunkid,
+IFNULL(fdtraninfo.groupunkid, 0) AS GroupId,
+IFNULL(trcontact.mobile, '') AS mobile,
+fdtraninfo.tranunkid,
+fdrentalinfo.rentalunkid,
+trcontact.custom3
+FROM
+saas_ezee.fdtraninfo
+    LEFT JOIN
+    saas_ezee.fdrentalinfo ON fdrentalinfo.tranunkid = fdtraninfo.tranunkid
+    AND fdrentalinfo.hotel_code = ?
+    AND IFNULL(fdrentalinfo.ismodifiedfromOTA, 0) <> 1
+    AND fdrentalinfo.statusunkid NOT IN (8 , 12)
+    AND fdrentalinfo.rentaldate = CASE
+    WHEN fdtraninfo.statusunkid IN (1 , 3, 11, 14) THEN '2022-06-07'
+    ELSE CAST(fdtraninfo.arrivaldatetime AS DATE)
+END
+    INNER JOIN
+    saas_ezee.fdguesttran ON fdguesttran.tranunkid = fdtraninfo.tranunkid
+    AND fdguesttran.hotel_code = ?
+    AND fdtraninfo.masterguesttranunkid = fdguesttran.guesttranunkid
+    INNER JOIN
+    saas_ezee.trcontact ON fdguesttran.guestunkid = trcontact.contactunkid
+    AND trcontact.hotel_code = ?
+    INNER JOIN
+    saas_ezee.cfroomtype ON cfroomtype.roomtypeunkid = fdrentalinfo.roomtypeunkid
+    AND cfroomtype.hotel_code = ?
+    LEFT JOIN
+    saas_ezee.cfroom ON cfroom.roomunkid = fdrentalinfo.roomunkid
+    AND cfroom.hotel_code = ?
+WHERE
+fdtraninfo.hotel_code = ?
+    AND fdtraninfo.tranunkid = ?
+GROUP BY fdtraninfo.tranunkid`;
+
+exports.hotel_map =`SELECT saas_ezee.cfhotel.*,saas_ezee.cfcountry.country_name FROM saas_ezee.cfhotel LEFT JOIN saas_ezee.cfcountry ON saas_ezee.cfcountry.countryunkid = saas_ezee.cfhotel.country  WHERE saas_ezee.cfhotel.hotel_code=?`;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,14 @@
-const { redirect } = require("express/lib/response");
 const jwt = require("jsonwebtoken");
 const { con } = require("../config/connection");
-const { checkProperty } = require("../functions/loginFunction");
-
+const {
+  checkHotel,
+  checkPin,
+  checkLanguage,
+  selectHotel_stuff,
+  check_currency,
+  userData,
+  hotel_map,
+} = require("../SQL/sqlQueries");
 exports.login = async (req, res) => {
   const { loginId, pin } = req.body;
 
@@ -10,25 +16,19 @@ exports.login = async (req, res) => {
     if (!loginId || !pin) {
       return res.status(401).json({ error: "Please fill all the fileds." });
     } else {
+      //assign variables
+      var door_code = pin;
+      var reservation_code = loginId;
       var hotel_code = 9074;
 
       // check property code
-      let sql = `SELECT databasename from saasconfig.syscompany where hotel_code=${hotel_code}`;
-      con.query(sql, (err, result) => {
+      con.query(checkHotel, [hotel_code], (err, result) => {
         if (err) throw err;
-        console.log("Result: " + result["0"]["databasename"]);
+        console.log("done");
 
-        const door_code = pin;
-        var reservation_code = loginId;
         // Check pin
-        //parameterized sql queries
-
-        sql = `SELECT fdtraninfo.tranunkid,fdtraninfo.statusunkid,fdrentalinfo.is_void_cancelled_noshow_unconfirmed AS isVoid,fdtraninfo.isgroupowner,fdtraninfo.allowselfguestportalaccess ,fdguesttran.guestunkid, fdtraninfo.supress_roomcharge from saas_ezee.fdtraninfo LEFT JOIN saas_ezee.fdrentalinfo ON fdtraninfo.tranunkid = fdrentalinfo.tranunkid AND fdrentalinfo.is_void_cancelled_noshow_unconfirmed=IF(fdtraninfo.statusunkid IN(5,6,7),1,0) AND ifnull(fdrentalinfo.ismodifiedfromOTA,0) <> 1 AND fdrentalinfo.statusunkid NOT IN (8,12) AND fdrentalinfo.hotel_code= ? LEFT JOIN saas_ezee.fdguesttran ON fdtraninfo.masterguesttranunkid = fdguesttran.guesttranunkid  AND fdguesttran.hotel_code= ? where IF(IFNULL(fdtraninfo.reservationno,'')='',TRIM(LEADING  FROM (TRIM(LEADING ? FROM fdtraninfo.tranunkid)))= ?,fdtraninfo.reservationno=?) AND 
-      fdtraninfo.doorcode=? AND fdtraninfo.hotel_code=? GROUP BY fdtraninfo.tranunkid `;
-
-        // console.log(sql)
         con.query(
-          sql,
+          checkPin,
           [
             hotel_code,
             hotel_code,
@@ -41,50 +41,45 @@ exports.login = async (req, res) => {
           (err, result) => {
             if (err) throw err;
             console.log(result);
+            console.log("done");
 
             var tranunkid = result[0].tranunkid; //tranukid
-            console.log("tarunkid ==========", tranunkid);
-            console.log("datalength", result.length);
             data = result.length;
 
-            // const tranunkid = result
-            // data=0
-
+            //checkLanguage
             if (data == 1) {
-              sql = `SELECT cfuser.language,cfuser.userunkid from saas_ezee.cfuser where hotel_code=${hotel_code} AND username='admin' AND sysdefined='ADMIN'`;
-              con.query(sql, (err, result) => {
-                if (err) throw err;
-                console.log("Result_last", result);
-                // return res.status(200).json({ data: result });
-
-                sql = `SELECT website,accountfor,logo FROM saas_ezee.cfhotel WHERE hotel_code=${hotel_code}`;
-
-                con.query(sql, (err, result) => {
+              con.query(
+                checkLanguage,
+                [hotel_code, "admin", "ADMIN"],
+                (err, result) => {
                   if (err) throw err;
-                  // console.log(result);
-                });
+                  console.log("done");
 
-                sql = `SELECT ER.exchangerateunkid,ER.isprefix AS Prefix,ER.currency_sign As Currency,ER.currency_code As Code,digits_after_decimal,ER.currency_name As Currency_Name,ER.exchange_rate1 as exchange_rate1,ER.exchange_rate2 as exchange_rate2 FROM saas_ezee.cfexchangerate AS ER  WHERE ER.isactive = 1 AND ER.hotel_code=${hotel_code}`;
-                con.query(sql, (err, result) => {
-                  if (err) throw err;
-                  // console.log(result);
-                  // res.status(200).json({ data: result });
+                  //selectHotel_stuff
+                  con.query(selectHotel_stuff, [hotel_code], (err, result) => {
+                    if (err) throw err;
+                    console.log(result);
+                  });
 
-                  //jwt token
+                  //check_currency
+                  con.query(check_currency, [hotel_code], (err, result) => {
+                    if (err) throw err;
 
-                  const token = jwt.sign(
-                    { userId: loginId },
-                    process.env.JWT_TOKEN,
-                    {
-                      expiresIn: "12h",
-                    }
-                  );
+                    //jwt token
+                    const token = jwt.sign(
+                      { userId: loginId },
+                      process.env.JWT_TOKEN,
+                      {
+                        expiresIn: "12h",
+                      }
+                    );
 
-                  res
-                    .status(200)
-                    .json({ token, message: "User Login SucessFully" });
-                });
-              });
+                    res
+                      .status(200)
+                      .json({ token, message: "User Login SucessFully" });
+                  });
+                }
+              );
             } else {
               res.status(401).json({ error: "You are not authorized user" });
             }
@@ -96,8 +91,8 @@ exports.login = async (req, res) => {
     console.log(e);
   }
 };
-// middleware
 
+// middleware
 exports.requireSignin = async (req, res, next) => {
   const token = req.headers.authorization;
   try {
@@ -110,111 +105,34 @@ exports.requireSignin = async (req, res, next) => {
 };
 
 //userDashboard
-
 exports.userDashboard = async (req, res) => {
   // res.status(200).json({ message: "Login Sucessfully", data_acess: 1 });
   let tranunkid = `907400000000000011`;
   const hotel_code = 9074;
   const reservation_code = 9;
-  sql = `SELECT 
-  IFNULL(CASE
-              WHEN IFNULL(fdtraninfo.groupunkid, 0) = 0 THEN fdtraninfo.reservationno
-              ELSE (CASE
-                  WHEN IFNULL(fdtraninfo.subreservationno, 0) = 0 THEN fdtraninfo.reservationno
-                  ELSE CONCAT(fdtraninfo.reservationno,
-                          '-',
-                          fdtraninfo.subreservationno)
-              END)
-          END,
-          '') AS reservationno,
-  TRIM(LEADING '0' FROM (TRIM(LEADING ${hotel_code} FROM fdtraninfo.tranunkid))) AS loginid,
-  fdtraninfo.arrivaldatetime AS arrival,
-  fdtraninfo.masterfoliounkid AS foliounkid,
-  fdtraninfo.departuredatetime AS departure,
-  travelagentvoucherno,
-  CONCAT(IFNULL(trcontact.salutation, ''),
-          ' ',
-          trcontact.name) AS name,
-  trcontact.email AS notiemail,
-  fdtraninfo.doorcode AS notidoorcode,
-  fdtraninfo.reservationno AS notiresno,
-  IFNULL((SELECT 
-                  lookupunkid
-              FROM
-                  saas_ezee.cfcommonlookup AS cfcommonlookup
-              WHERE
-                  hotel_code = ${hotel_code}
-                      AND lookuptype = 'SALUTATION'
-                      AND (cfcommonlookup.lookup = trcontact.salutation)
-              LIMIT 1),
-          '') AS salutation,
-  fdrentalinfo.adult,
-  fdrentalinfo.child,
-  cfroomtype.roomtype AS RoomType,
-  cfroom.name AS Roomno,
-  fdtraninfo.statusunkid,
-  fdtraninfo.isgroupowner,
-  fdrentalinfo.ratetypeunkid,
-  DATEDIFF(fdtraninfo.departuredatetime,
-          fdtraninfo.arrivaldatetime) AS noofdays,
-  CAST(fdtraninfo.arrivaldatetime AS DATE) AS arrivaldate,
-  CAST(fdtraninfo.departuredatetime AS DATE) AS departuredate,
-  fdrentalinfo.childage,
-  fdtraninfo.ispaymentlinkamountset,
-  fdtraninfo.googleshortlink,
-  fdtraninfo.lang_key,
-  fdtraninfo.sourceunkid,
-  IFNULL(fdtraninfo.groupunkid, 0) AS GroupId,
-  IFNULL(trcontact.mobile, '') AS mobile,
-  fdtraninfo.tranunkid,
-  fdrentalinfo.rentalunkid,
-  trcontact.custom3
-FROM
-  saas_ezee.fdtraninfo
-      LEFT JOIN
-      saas_ezee.fdrentalinfo ON fdrentalinfo.tranunkid = fdtraninfo.tranunkid
-      AND fdrentalinfo.hotel_code = ${hotel_code}
-      AND IFNULL(fdrentalinfo.ismodifiedfromOTA, 0) <> 1
-      AND fdrentalinfo.statusunkid NOT IN (8 , 12)
-      AND fdrentalinfo.rentaldate = CASE
-      WHEN fdtraninfo.statusunkid IN (1 , 3, 11, 14) THEN '2022-06-07'
-      ELSE CAST(fdtraninfo.arrivaldatetime AS DATE)
-  END
-      INNER JOIN
-      saas_ezee.fdguesttran ON fdguesttran.tranunkid = fdtraninfo.tranunkid
-      AND fdguesttran.hotel_code = ${hotel_code}
-      AND fdtraninfo.masterguesttranunkid = fdguesttran.guesttranunkid
-      INNER JOIN
-      saas_ezee.trcontact ON fdguesttran.guestunkid = trcontact.contactunkid
-      AND trcontact.hotel_code = ${hotel_code}
-      INNER JOIN
-      saas_ezee.cfroomtype ON cfroomtype.roomtypeunkid = fdrentalinfo.roomtypeunkid
-      AND cfroomtype.hotel_code = ${hotel_code}
-      LEFT JOIN
-      saas_ezee.cfroom ON cfroom.roomunkid = fdrentalinfo.roomunkid
-      AND cfroom.hotel_code = ${hotel_code}
-WHERE
-  fdtraninfo.hotel_code = ${hotel_code}
-      AND fdtraninfo.tranunkid = ${tranunkid}
-GROUP BY fdtraninfo.tranunkid`;
 
-  con.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-
-    // var tranunkid = result[0].tranunkid;
-
-    // console.log("_______________________________", tranunkid);
-
-    res.status(200).json({ message: result });
-  });
+  con.query(
+    userData,
+    [
+      hotel_code,
+      hotel_code,
+      hotel_code,
+      hotel_code,
+      hotel_code,
+      hotel_code,
+      hotel_code,
+      hotel_code,
+      tranunkid,
+    ],
+    (err, result) => {
+      if (err) throw err;
+      res.status(200).json({ message: result });
+    }
+  );
 };
 
 exports.hotelMap = async (req, res) => {
-  const hotel_code = 9074;
-
-  var sql = `SELECT saas_ezee.cfhotel.*,saas_ezee.cfcountry.country_name FROM saas_ezee.cfhotel LEFT JOIN saas_ezee.cfcountry ON saas_ezee.cfcountry.countryunkid = saas_ezee.cfhotel.country  WHERE saas_ezee.cfhotel.hotel_code=9075`;
-  con.query(sql, (err, result) => {
+  con.query(hotel_map, [hotel_code], (err, result) => {
     if (err) throw err;
     console.log(result);
 
