ed920e989169b95708f57e63ac9eeb142b4d011f
darshan <darshan.mistry@ezeetechnosys.com>
2022-06-15 16:04:14 +0530

    new1

--- config/connection.js
+++ config/connection.js
@@ -10,27 +10,26 @@ var db_config = {
   dateStrings: ["DATE", "DATETIME"],
 };
 
-var con;
-
+var connection;
 function handleDisconnect() {
-  con = mysql.createConnection(db_config);
+  connection = mysql.createConnection(db_config); 
 
-  (function (err) {
-    if (err) {
-      console.log("error when connecting to db:", err);
-      setTimeout(handleDisconnect, 2000);
-    }
-  });
-
-  con.on("error", function (err) {
-    console.log("db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect();
-    } else {
-      throw err;
+  (function(err) {             
+    if(err) {                                    
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  })();                                     
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                        
+    } else {                                      
+      throw err;                                  
     }
   });
 }
+handleDisconnect(connection);
 
-handleDisconnect();
-exports.con = con;
+
+exports.con = connection;
